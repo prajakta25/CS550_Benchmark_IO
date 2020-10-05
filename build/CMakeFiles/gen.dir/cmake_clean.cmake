14930bff89ebf4678407e4f2b787d179c10799c6
PSP20SCM25R <prajhans@hawk.iit.edu>
2020-10-05 15:16:34 -0500

    update

---
+++ build/CMakeFiles/gen.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gen.dir/workload_generator.cpp.o"
+  "gen"
+  "gen.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
