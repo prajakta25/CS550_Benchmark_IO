9bf302cced906946bd7db19b60453688a44ca34d
prajakta25 <prajaktarajhans96@gmail.com>
2020-10-06 11:51:47 -0500

    cmake changes

--- build/CMakeFiles/Makefile.cmake
+++ build/CMakeFiles/Makefile.cmake
@@ -42,5 +42,5 @@ set(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
-  "CMakeFiles/gen.dir/DependInfo.cmake"
+  "CMakeFiles/generator.dir/DependInfo.cmake"
   )
--- build/CMakeFiles/generator.dir/DependInfo.cmake
+++ build/CMakeFiles/generator.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/prajakta/CS550_Benchmark_IO/workload_generator.cpp" "/home/prajakta/CS550_Benchmark_IO/build/CMakeFiles/gen.dir/workload_generator.cpp.o"
+  "/home/prajakta/CS550_Benchmark_IO/workload_generator.cpp" "/home/prajakta/CS550_Benchmark_IO/build/CMakeFiles/generator.dir/workload_generator.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
